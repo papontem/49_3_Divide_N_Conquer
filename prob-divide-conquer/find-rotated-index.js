e7587cfe6152bfd39902ac81b03a00e3001cba8d
papontemjetix <papontem@gmail.com>
2024-01-02 20:55:46 -0500

    thinking....

--- prob-divide-conquer/find-rotated-index.js
+++ prob-divide-conquer/find-rotated-index.js
@@ -10,12 +10,13 @@
  *
  * Examples:
  *
- *     findRotatedIndex([3,4,1,2],4) // 1
- *     findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 8) // 2
- *     findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 3) // 6
- *     findRotatedIndex([37,44,66,102,10,22],14) // -1
- *     findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
+ *     findRotatedIndex(  [        3 , 4 , 1 , 2 ] , 4 ) // 1    [1,2,3,4] -rotatedX2 clock or counter >   [3,4,1,2]
+ *     findRotatedIndex(  [6, 7, 8, 9, 1, 2, 3, 4] , 8 ) // 2    [1,2,3,4,6,7,8,9]  -X4 counter> [6, 7, 8, 9, 1, 2, 3, 4]
+ *     findRotatedIndex(  [6, 7, 8, 9, 1, 2, 3, 4] , 12) // -1	 [1,2,3,4,6,7,8,9]  -X4 counter> [6, 7, 8, 9, 1, 2, 3, 4]
+ *     findRotatedIndex(  [6, 7, 8, 9, 1, 2, 3, 4] , 3 ) // 6	 [1,2,3,4,6,7,8,9]  -X4 counter> [6, 7, 8, 9, 1, 2, 3, 4]
+ *     findRotatedIndex(  [37, 44, 66, 102, 10, 22], 14) // -1   [10,22,37,44,66,102] -X2 counter> [37, 44, 66, 102, 10, 22]
  *
+ * PAM: array rotation means we are shifting array index around either clock wise (right) or counter-clockwise (left)
  */
 //       v
 // [ 3 , 4 , 1 , 2 ] > 4 <
@@ -41,7 +42,7 @@
 //                 |   v   |
 // [ 37, 44, 66, 102, 10, 22]
 //                        |v|
-// [ 37, 44, 66, 102, 10, 22]
+// [ 37, 44, 66, 102, 10, 22]  l&R meet but no 14 found returns -1
 
 function findRotatedIndex(arr, n) {
 	let leftIdx = 0;
@@ -52,23 +53,10 @@ function findRotatedIndex(arr, n) {
 	while (leftIdx <= rightIdx) {
 		let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
 		let middleVal = arr[middleIdx];
-		console.log("MIddle Index:", middleIdx);
-		console.log("MIddle Val:", middleVal);
-		console.log(
-			"L",
-			leftIdx,
-			"R",
-			rightIdx,
-			"middleIdx",
-			middleIdx,
-			"MidVal",
-			middleVal,
-			"target:",
-			n
-		);
+		
 
 		if (middleVal == n) {
-			console.log("RETURNING :", middleVal);
+			// console.log("RETURNING :", middleVal);
 			return middleIdx;
 		}
 
@@ -97,36 +85,8 @@ function findRotatedIndex(arr, n) {
 		}
 	}
 	// value was never found
-	console.log("NOT FOUND RETURNING -1");
+	// console.log("NOT FOUND RETURNING -1");
 	return -1;
 }
 
 module.exports = findRotatedIndex;
-
-// demo/binarySearch.js
-
-// function binarySearch(arr, val) {
-
-//   let leftIdx = 0;
-//   let rightIdx = arr.length - 1;
-
-//   while (leftIdx <= rightIdx) {
-//     // find the middle value
-//     let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
-//     let middleVal = arr[middleIdx];
-
-//     if (middleVal < val) {
-//       // middleVal is too small, look at the right half
-//       leftIdx = middleIdx + 1;
-//     } else if (middleVal > val) {
-//       // middleVal is too large, look at the left half
-//       rightIdx = middleIdx - 1;
-//     } else {
-//       // we found our value!
-//       return middleIdx;
-//     }
-//   }
-
-//   // left and right pointers crossed, val isn't in arr
-//   return -1;
-// }
--- prob-divide-conquer/find-rotation-count.js
+++ prob-divide-conquer/find-rotation-count.js
@@ -1,22 +1,72 @@
 // findRotationCount.js
 /**
- * 
+ *
  * findRotationCount
- * 
+ *
  * Write a function called findRotationCount which accepts an array of distinct numbers sorted in increasing order.
  * The array has been rotated counter-clockwise n number of times. Given such an array, find the value of n.
- * 
+ *
  * Constraints: Time Complexity: O(log N)
- * 
+ *
  * Examples:
- * 
- *     findRotationCount([15, 18, 2, 3, 6, 12]) // 2
- *     findRotationCount([7, 9, 11, 12, 5]) // 4
- *     findRotationCount([7, 9, 11, 12, 15]) // 0
- * 
+ *                 indx:   0   1   2   3   4   5
+ *     findRotationCount([ 7,  9, 11, 12, 15    ]) // 0      orig: [7,9,11,12,15]   ->   not rotated counter= [7, 9, 11, 12, 15]
+ *     findRotationCount([15, 18,  2,  3,  6, 12]) // 2      orig: [2,3,6,12,15,18] -> rotated twice counter= [15, 18, 2, 3, 6, 12]
+ *     findRotationCount([ 7,  9, 11, 12,  5    ]) // 4      orig: [5,7,9,11,12]    -> rotated  X 4  counter= [7, 9, 11, 12, 5]
+ *
+ * PAM: the array's smallest number will be the clue to finding out how many times a sorted array has been rotated counter clockwise.
+ * It baisically tells us how many times a number has been put in front of it.
  */
-function findRotationCount() {
-  
+// PAM: this is just a findMin() function in disguise
+function findRotationCount(arr) {
+	let left = 0;
+	let right = arr.length - 1;
+    let count;
+	while (left <= right) {
+		let middleIdx = Math.floor((left + right) / 2);
+		let middleVal = arr(middleIdx);
+		console.log("MIddle Index:", middleIdx);
+		console.log("MIddle Val:", middleVal);
+		console.log(
+			"L",
+			leftIdx,
+			"R",
+			rightIdx,
+			"middleIdx",
+			middleIdx,
+			"MidVal",
+			middleVal
+		);
+
+	}
 }
 
-module.exports = findRotationCount
+module.exports = findRotationCount;
+
+// demo/binarySearch.js
+
+// function binarySearch(arr, val) {
+
+//   let leftIdx = 0;
+//   let rightIdx = arr.length - 1;
+
+//   while (leftIdx <= rightIdx) {
+//     // find the middle value
+//     let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+//     let middleVal = arr[middleIdx];
+
+//     if (middleVal < val) {
+//       // middleVal is too small, look at the right half
+//       leftIdx = middleIdx + 1;
+//     } else if (middleVal > val) {
+//       // middleVal is too large, look at the left half
+//       rightIdx = middleIdx - 1;
+//     } else {
+//       // we found our value!
+//       return middleIdx;
+//     }
+//   }
+
+//   // left and right pointers crossed, val isn't in arr
+//   return -1;
+// }
